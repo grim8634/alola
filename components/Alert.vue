01c175d340270331c810471cd03ec9ead9d97759
Graeme Lawton <graeme.lawton@cartridgesave.co.uk>
2025-01-07 17:57:10 +0000

    task: added some initial stying stuff

---
+++ components/Alert.vue
@@ -0,0 +1,15 @@
+<template>
+    <div class="bg-green-500 roudned px-2 py-1 text-white">
+        This is an alert component.
+    </div>
+</template>
+
+<script>
+    export default {
+        
+    }
+</script>
+
+<style lang="scss" scoped>
+
+</style>
---
+++ pages/events/[id].vue
@@ -0,0 +1,14 @@
+<template>
+    <div>
+        Event Page for event {{ $route.params.id }}
+    </div>
+</template>
+
+<script setup>
+    const nuxt = useNuxtApp();
+
+</script>
+
+<style lang="scss" scoped>
+
+</style>
